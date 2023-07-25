e0ea85f5b576c6fcd0267a1ce620b8fa899ee5dd
shihabhassannaim <hassanshihab231@gmail.com>
2023-07-25 12:33:49 +0600

    again

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header/Header';
+import Shop from './components/Shop/Shop';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Review from './components/Review/Review';
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      <Router>
+        <Switch>
+          <Route path="/shop">
+            <Shop></Shop>
+          </Route>
+          <Route path="/review">
+            <Review></Review>
+          </Route>
+        </Switch>
+      </Router>
+
+
+    </div>
+  );
+}
+
+export default App;
